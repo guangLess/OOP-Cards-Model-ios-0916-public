8be717e9328ca5dde455489f2dcb2439bfab99fe
guangLess <bha.guang@gmail.com>
2015-10-06 13:35:32 -0400

    Car class finished testing

--- OOP-Cards-Model/FISAppDelegate.m
+++ OOP-Cards-Model/FISAppDelegate.m
@@ -1,11 +1,15 @@
 //  FISAppDelegate.m
 
 #import "FISAppDelegate.h"
+#import "FISCard.h"
 
 @implementation FISAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    FISCard * cardN = [[FISCard alloc] init];
+    NSLog(@"carN = %@", [cardN description]);
     return YES;
 }
 
---
+++ OOP-Cards-Model/FISCardDeck.m
@@ -0,0 +1,36 @@
+//
+//  FISCardDeck.m
+//  OOP-Cards-Model
+//
+//  Created by Guang on 10/5/15.
+//  Copyright © 2015 Al Tyus. All rights reserved.
+//
+
+#import "FISCardDeck.h"
+
+@implementation FISCardDeck
+
+
+
+-(instancetype)init{
+    
+    self = [super self];
+    return self;
+}
+
+-(FISCard *)drawNextCard{
+    return nil;
+}
+
+-(void)resetDeck{
+    
+    
+}
+-(void)gatherDealtCards{
+    
+}
+-(void)shuffleRemainingCards{
+    
+}
+
+@end
